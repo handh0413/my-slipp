ecffdd00f900e6e657afe92e84184e7148f42a80
donghee <handh0413@gmail.com>
2020-01-07 22:15:06 +0900

    5-2. 질문 상세보기 기능

--- src/main/java/net/slipp/web/QuestionController.java
+++ src/main/java/net/slipp/web/QuestionController.java
@@ -4,7 +4,9 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -36,7 +38,13 @@ public class QuestionController {
 		User sessionedUser = HttpSessionUtils.getUserFromSession(session);
 		Question newQuestion = new Question(sessionedUser, title, contents);
 		questionRepository.save(newQuestion);
-		
 		return "redirect:/";
+	}
+	
+	@GetMapping("/{id}")
+	public String show(@PathVariable Long id, Model model) {
+		Question question = questionRepository.findById(id).get();
+		model.addAttribute("question", question);
+		return "/qna/show";
 	}
 }
